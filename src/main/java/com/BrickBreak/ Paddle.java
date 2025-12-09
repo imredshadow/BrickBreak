9e4a84c73288a7b3ab9b5112c131e04b57b85b7d
imredshadow <jianturalbaacol@gmail.com>
2025-12-09 19:28:38 +0000

    67

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -7,15 +7,70 @@ public class Paddle {
 
 
 	//your code here!
-
-
-	
-	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int height;
+	private int speed;
 	
 	//constructor(s):
-	
+	public Paddle(int x, int y, int height, int speed) {
+		this.x = x;
+		this.y = y;
+		this.height = height;
+		this.speed = speed;
+	}
+
 	//methods:
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getYpos() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void setXVelocity(int x) {
+		this.xVelocity = x;
+	}
+
+	public void setYVelocity(int y) {
+		this.yVelocity = y;
+	}
+
+	public void reverseX() {
+		xVelocity *= -1;
+	}
+
+	public void reverseY() {
+		yVelocity *= -1;
+	}
+
+	public void draw(Graphics g){
+		g.setColor(Color.cyan);
+		g.fillOval(x, y, size, size);
+	}
+
+	public void move() {
+		x += xVelocity;
+		y += yVelocity;
+	}
+
 	
 	
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,3 +1,6 @@
+//Jian Acol
+//Creates the Ball class and its instance variables, constructors, and methods.
+
 package com.BrickBreak;
 import java.awt.*;
 
@@ -5,11 +8,69 @@ public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int x;
+	private int y;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
 	
 	//constructor(s):
-	
+	public Ball(int x, int y, int size) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+	}
+
 	//methods:
+	public int getXpos() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getYpos() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void setXVelocity(int x) {
+		this.xVelocity = x;
+	}
+
+	public void setYVelocity(int y) {
+		this.yVelocity = y;
+	}
+
+	public void reverseX() {
+		xVelocity *= -1;
+	}
+
+	public void reverseY() {
+		yVelocity *= -1;
+	}
+
+	public void draw(Graphics g){
+		g.setColor(Color.cyan);
+		g.fillOval(x, y, size, size);
+	}
+
+	public void move() {
+		x += xVelocity;
+		y += yVelocity;
+	}
+
 
 }
